045fa6279feba77e427a74ef109770462ed154f1
monkeh42 <72186713+monkeh42@users.noreply.github.com>
2021-08-16 23:17:02 -0400

    Update prod.js

--- js/prod.js
+++ js/prod.js
@@ -5,7 +5,7 @@ var PROD_DATA = {
     upgrades: {
         className: 'prod-upg',
         isBought: (id) => false,
-        canAfford: (id) => player.money.gte(this[id].cost()),
+        canAfford: (id) => player.money.gte(DATA.p.upgrades[id].cost()),
         1: {
             title: 'First Producer',
             desc: () => 'Produces 1 resource per second.',
